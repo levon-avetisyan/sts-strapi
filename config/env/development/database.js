c392aa495fd3840b1cb16939d46f287c2bdd0046
Levon <levon.s.avetisyan@gmail.com>
2025-08-20 12:29:04 +0400

    deploy fix

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
     connection: {
+        client: 'postgres',
         connection: {
             connectionString: env('DATABASE_PUBLIC_URL')
-        }
+        },
+        debug: true,
+        pool: { min: 0, max: 7 },
     }
 });
